0ad48aa8567d23a445e7d58ac62c033a22c38202
LakshMapare <98962825+LakshMapare@users.noreply.github.com>
2022-02-04 23:32:38 +0530

    Update ActitimeHeader.java

--- Automation_Applicatin/src/main/java/Pom/ActitimeHeader.java
+++ Automation_Applicatin/src/main/java/Pom/ActitimeHeader.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-
+//Checking GitHUb Changes
 public class ActitimeHeader {
 	@FindBy(xpath ="//a[@class='content users']")
 	private WebElement user;
